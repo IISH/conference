c0c0012c8512e191c942719ea686aa8a0372cafb
Kerim <kerim.meijer@gmail.com>
2013-07-31 09:29:54 +0200

    #320: XML exports are now properly escaped

--- src/groovy/org/iisg/eca/export/XmlExport.groovy
+++ src/groovy/org/iisg/eca/export/XmlExport.groovy
@@ -1,12 +1,14 @@
 package org.iisg.eca.export
 
 import groovy.xml.MarkupBuilder
+import java.util.regex.Pattern
 
 /**
  * Export xml files
  */
 class XmlExport extends AbstractExport {
     private static final String CONTENT_TYPE = 'application/xml'
+    private static final Pattern INVALID_CHARS = Pattern.compile("[^a-zA-Z0-9]")
 
     /**
      * Creates a new xml export for the specified element
@@ -38,18 +40,28 @@ class XmlExport extends AbstractExport {
 
         writer.write('<?xml version="1.0" encoding="utf-8" ?>\n')
 
-        xml."${title.toLowerCase().replaceAll('\\s', '-')}"() {
+        xml."${escapeString(title.toLowerCase())}"() {
             results.eachWithIndex { r, i ->
-                xml.row("${UTILS.message(code: 'default.count').toLowerCase()}": i+1) {
+                xml.row("${escapeString(UTILS.message(code: 'default.count').toLowerCase())}": i+1) {
                     columns.grep { it.canBeShown() }.eachWithIndex { c, j ->
                         def value = r
                         c.columnPath.each { value = value[it.toString()] }
-                        xml."${columnNames[j].toLowerCase().replaceAll('\\s', '-')}"(value)
+                        xml."${escapeString(columnNames[j].toLowerCase())}"(value)
                     }
                 }
             }
         }
 
         writer
+    }
+
+    /**
+     * Makes sure all illegal characters are replaced by an underscore (_)
+     * @param text The text which may contain illegal characters
+     * @return The valid text to use in xml document
+     */
+    private String escapeString(String text) {
+        text = text.trim()
+        INVALID_CHARS.matcher(text).replaceAll("_")
     }
 }
