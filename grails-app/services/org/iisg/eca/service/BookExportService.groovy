a5f818e607e2cfb4a0f6cfd856eeb03634174520
Kerim <kerim.meijer@gmail.com>
2013-09-16 14:08:05 +0200

    Improved export service

--- grails-app/services/org/iisg/eca/service/BookExportService.groovy
+++ grails-app/services/org/iisg/eca/service/BookExportService.groovy
@@ -7,11 +7,12 @@ import groovy.xml.MarkupBuilder
 
 import org.iisg.eca.domain.Day
 import org.iisg.eca.domain.User
-import org.iisg.eca.domain.SessionDateTime
-import org.iisg.eca.domain.Session
 import org.iisg.eca.domain.Room
-import org.iisg.eca.domain.Network
 import org.iisg.eca.domain.Paper
+import org.iisg.eca.domain.Network
+import org.iisg.eca.domain.Session
+import org.iisg.eca.domain.ParticipantType
+import org.iisg.eca.domain.SessionDateTime
 import org.iisg.eca.domain.ParticipantState
 
 /**
@@ -20,6 +21,10 @@ import org.iisg.eca.domain.ParticipantState
 class BookExportService {
     def pageInformation
 
+    /**
+     * Creates an XML with all the conference days
+     * @return A string representing the XML
+     */
     String getDaysXml() {
         DateFormat weekDayFormatter = new SimpleDateFormat('EEEEE', Locale.US)
         DateFormat dayFormatter = new SimpleDateFormat('dd', Locale.US)
@@ -50,6 +55,10 @@ class BookExportService {
         }
     }
 
+    /**
+     * Creates an XML with all the participants
+     * @return A string representing the XML
+     */
     String getConcordanceXml() {
         getXml("Export of Concordance for ${getEventCode()} Programbook") { builder ->
             builder.names {
@@ -73,6 +82,10 @@ class BookExportService {
         }
     }
 
+    /**
+     * Creates an XML with all the sessions
+     * @return A string representing the XML
+     */
     String getSessionsXml() {
         DateFormat weekDayFormatter = new SimpleDateFormat('EEEEE', Locale.US)
         DateFormat dayFormatter = new SimpleDateFormat('dd', Locale.US)
@@ -80,16 +93,16 @@ class BookExportService {
         DateFormat yearFormatter = new SimpleDateFormat('yyyy', Locale.US)
 
         getXml("Export ${getEventCode()} Programbook (Sessions) (English)") { builder ->
-            builder.sessions {
-                Day.executeQuery('''
-                    SELECT d, sdt
-                    FROM Day AS d
-                    INNER JOIN d.sessionDateTimes AS sdt
-                    ORDER BY d.id, sdt.indexNumber
-                ''').each { results ->
-                    Day d = results[0]
-                    SessionDateTime sdt = results[1]
+            Day.executeQuery('''
+                SELECT d, sdt
+                FROM Day AS d
+                INNER JOIN d.sessionDateTimes AS sdt
+                ORDER BY d.id, sdt.indexNumber
+            ''').each { results ->
+                Day d = results[0]
+                SessionDateTime sdt = results[1]
 
+                builder.sessions {
                     builder.time {
                         builder.weekday(weekDayFormatter.format(d.day))
                         builder.year(yearFormatter.format(d.day))
@@ -105,118 +118,37 @@ class BookExportService {
                         SELECT s, r
                         FROM Session AS s
                         INNER JOIN s.sessionRoomDateTime AS srdt
-                        INNER JOIN srdt.sessionDateTime AS sdt
                         INNER JOIN srdt.room AS r
-                        WHERE sdt.id = :sessionDateTimeId
-                        AND s.enabled = true
-                        AND s.deleted = false
-                        AND srdt.enabled = true
+                        WHERE srdt.sessionDateTime.id = :sessionDateTimeId
                         AND srdt.deleted = false
-                        AND r.enabled = true
                         AND r.deleted = false
+                        AND n.deleted = false
                         ORDER BY r.roomNumber
                     ''', [sessionDateTimeId: sdt.id]).each { sessionResults ->
-                        Session session = sessionResults[0]
-                        Room room = sessionResults[1]
+                        Session session = (Session) sessionResults[0]
+                        Room room = (Room) sessionResults[1]
 
-                        builder.session {
-                            //builder.code(session.code)
-                            builder.sessionname(session.name)
-
-                            builder.location {
-                                builder.code("${room.roomNumber}-${sdt.indexNumber}")
-                                builder.locationname(room.roomName)
-                            }
-
-                            builder.networks {
-                                Network.executeQuery('''
-                                    SELECT n
-                                    FROM Network AS n
-                                    INNER JOIN n.sessions AS s
-                                    WHERE s.id = :sessionId
-                                    AND n.enabled = true
-                                    ORDER BY n.name
-                                ''', [sessionId: session.id]).each { network ->
-                                    builder.networkname(network.name)
-                                }
-                            }
-
-                            builder.chairs {
-                                User.executeQuery('''
-                                    SELECT u
-                                    FROM User AS u
-                                    INNER JOIN u.sessionParticipants AS sp
-                                    WHERE sp.session.id = :sessionId
-                                    AND sp.type.id = 6
-                                    AND u.enabled = true
-                                    ORDER BY u.lastName, u.firstName
-                                ''', [sessionId: session.id]).each { participant ->
-                                    builder.chairname("$participant.firstName $participant.lastName")
-                                }
-                            }
-
-                            builder.organizers {
-                                User.executeQuery('''
-                                    SELECT u
-                                    FROM User AS u
-                                    INNER JOIN u.sessionParticipants AS sp
-                                    WHERE sp.session.id = :sessionId
-                                    AND sp.type.id = 7
-                                    AND u.enabled = true
-                                    ORDER BY u.lastName, u.firstName
-                                ''', [sessionId: session.id]).each { participant ->
-                                    builder.organizername("$participant.firstName $participant.lastName")
-                                }
-                            }
-
-                            builder.discussants {
-                                User.executeQuery('''
-                                    SELECT u
-                                    FROM User AS u
-                                    INNER JOIN u.sessionParticipants AS sp
-                                    WHERE sp.session.id = :sessionId
-                                    AND sp.type.id = 10
-                                    AND u.enabled = true
-                                    ORDER BY u.lastName, u.firstName
-                                ''', [sessionId: session.id]).each { participant ->
-                                    builder.discussantname("$participant.firstName $participant.lastName")
-                                }
-                            }
-
-                            builder.papers {
-                                Paper.executeQuery('''
-                                    SELECT p, u
-                                    FROM Paper AS p
-                                    INNER JOIN p.user AS u
-                                    INNER JOIN u.sessionParticipants AS sp
-                                    WHERE sp.session.id = :sessionId
-                                    AND sp.type.id = 8
-                                    AND p.enabled = true
-                                    AND u.enabled = true
-                                    AND u.deleted = false
-                                    ORDER BY u.lastName, u.firstName
-                                ''', [sessionId: session.id]).each { paperInfo ->
-                                    Paper paper = paperInfo[0]
-                                    User user = paperInfo[1]
-
-                                    builder.paper {
-                                        builder.presenter("$user.firstName $user.lastName")
-                                        builder.copresenters(paper.coAuthors)
-                                        builder.subject(paper.title)
-                                    }
-                                }
-                            }
-                        }
+                        forEachSession(session, room, builder)
                     }
                 }
             }
         }
     }
 
+    /**
+     * The event code to use in the header of the XML
+     * @return A string representing the event in question
+     */
     private String getEventCode() {
         "${pageInformation.date.event.shortName}${pageInformation.date.yearCode}"
     }
 
+    /**
+     * Sets up the ground work for creating XML exports
+     * @param description The description to print in the XML
+     * @param body The body contents of the XML
+     * @return A string with the complete XML string
+     */
     private String getXml(String description, Closure body) {
         DateFormat formatter = new SimpleDateFormat('yyyy-MM-dd HH:mm:ss', Locale.US)
         StringWriter writer = new StringWriter()
@@ -233,5 +165,81 @@ class BookExportService {
         }
 
         writer.toString()
+    }
+
+    /**
+     * Creates the XML content for each session
+     * @param session The session to create the xml for
+     * @param room The room of the session
+     * @param builder The builder for creating the XML
+     */
+    private void forEachSession(Session session, Room room, MarkupBuilder builder) {
+        builder.session {
+            builder.sessionname(session.name)
+
+            builder.location {
+                builder.code("${room.roomNumber}-${sdt.indexNumber}")
+                builder.locationname(room.roomName)
+            }
+
+            builder.networks {
+                Network.executeQuery('''
+                    SELECT n
+                    FROM Network AS n
+                    INNER JOIN n.sessions AS s
+                    WHERE s.id = :sessionId
+                    ORDER BY n.name
+                ''', [sessionId: session.id]).each { network ->
+                    builder.networkname(network.name)
+                }
+            }
+
+            Map<ParticipantType, List<User>> sessionParticipants = new HashMap<ParticipantType, List<User>>()
+
+            User.executeQuery('''
+                SELECT u, t
+                FROM User AS u
+                INNER JOIN u.sessionParticipants AS sp
+                INNER JOIN sp.type AS t
+                WHERE sp.session.id = :sessionId
+                AND t.withPaper = false
+                ORDER BY t.importance desc, u.lastName, u.firstName
+            ''', [sessionId: session.id]).each { participant ->
+                User user = (User) participant[0]
+                ParticipantType type = (ParticipantType) participant[1]
+
+                List<User> users = sessionParticipants.get(type, new ArrayList<User>())
+                users.add(user)
+            }
+
+            sessionParticipants.each { type, users ->
+                builder."${type.type}s" {
+                    users.each { user ->
+                        builder."${type.type}name"("$user.firstName $user.lastName")
+                    }
+                }
+            }
+
+            builder.papers {
+                Paper.executeQuery('''
+                    SELECT p, u
+                    FROM Paper AS p
+                    INNER JOIN p.user AS u
+                    INNER JOIN u.sessionParticipants AS sp
+                    WHERE sp.session.id = :sessionId
+                    AND u.deleted = false
+                    ORDER BY u.lastName, u.firstName
+                ''', [sessionId: session.id]).each { paperInfo ->
+                    Paper paper = (Paper) paperInfo[0]
+                    User user = (User) paperInfo[1]
+
+                    builder.paper {
+                        builder.presenter("$user.firstName $user.lastName")
+                        builder.copresenters(paper.coAuthors)
+                        builder.subject(paper.title)
+                    }
+                }
+            }
+        }
     }
 }
