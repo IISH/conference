3823c5579bc19e67e09def405f611d9d38db5698
kerim1 <kerim.meijer@gmail.com>
2013-12-03 21:12:08 +0100

    Test test case

---
+++ test/unit/EmptyTests.groovy
@@ -0,0 +1,11 @@
+import grails.test.mixin.TestFor
+import org.iisg.eca.controller.EventController
+
+@TestFor(EventController)
+class EmptyTests {
+
+    // Temp test method
+    void testTemp() {
+        assert true
+    }
+}
