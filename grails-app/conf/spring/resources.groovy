38efc13f59a8316ef712e2c6c6d8109809e718f9
kerim1 <kerim.meijer@gmail.com>
2014-03-04 15:33:27 +0100

    Resources commit

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,14 +1,11 @@
-import org.iisg.eca.utils.PageInformation
-import org.iisg.eca.utils.CustomPropertyEditorRegistrar
-
-import org.iisg.eca.converter.DateConverter
-
 import org.iisg.eca.security.UserSaltSource
 import org.iisg.eca.security.UserDetailsService
 import org.iisg.eca.security.SecurityEventListener
-import org.iisg.eca.converter.DateConverter
+
 import org.iisg.eca.utils.PageInformation
 import org.iisg.eca.utils.CustomPropertyEditorRegistrar
+
+import org.iisg.eca.converter.DateConverter
 
 import org.springframework.security.oauth2.provider.token.JdbcTokenStore
 import org.springframework.security.oauth2.provider.JdbcClientDetailsService
