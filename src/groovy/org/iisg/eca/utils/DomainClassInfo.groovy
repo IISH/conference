e1a95761ba2d767d5afc5a301b67915908dbf9fe
kerim1 <kerim.meijer@gmail.com>
2014-03-14 09:06:15 +0100

    Commit

--- src/groovy/org/iisg/eca/utils/DomainClassInfo.groovy
+++ src/groovy/org/iisg/eca/utils/DomainClassInfo.groovy
@@ -3,6 +3,8 @@ package org.iisg.eca.utils
 import org.codehaus.groovy.grails.commons.GrailsApplication
 import org.codehaus.groovy.grails.commons.GrailsDomainClass
 import org.codehaus.groovy.grails.commons.GrailsDomainClassProperty
+import org.codehaus.groovy.grails.validation.Constraint
+import org.codehaus.groovy.grails.validation.NullableConstraint
 
 class DomainClassInfo {
     private GrailsDomainClass domainClass
@@ -37,6 +39,15 @@ class DomainClassInfo {
         dcProperty.getType()
     }
 
+	Constraint getConstraintForProperty(String property, String constraint) {
+		return this.domainClass.constrainedProperties?.get(property)?.getAppliedConstraint(constraint)
+	}
+
+	boolean propertyAllowsNull(String property) {
+		NullableConstraint nullableConstraint = (NullableConstraint) getConstraintForProperty(property, 'nullable')
+		return nullableConstraint?.isNullable()
+	}
+
     DomainClassInfo getDomainClassInfoForProperty(String property) {
         GrailsDomainClassProperty dcProperty = this.getPropertyByName(property)
         return new DomainClassInfo(dcProperty.referencedDomainClass)
@@ -54,6 +65,11 @@ class DomainClassInfo {
                 return value?.equals('1') || value?.equalsIgnoreCase('true')
             case 'date':
                 return value?.isLong() ? new Date(value.toLong() * 1000) : null
+	        case 'string':
+		        if ((value == null) || value.trim().isEmpty()) {
+			        return this.propertyAllowsNull(property) ? null : ''
+		        }
+		        return value.trim()
             default:
                 return value
         }
