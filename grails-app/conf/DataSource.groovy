37fe4d113a46856d39da12fcbd681e7a30a3aff8
kerim1 <kerim.meijer@gmail.com>
2014-03-19 08:44:37 +0100

    Fix h2 data source

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -75,7 +75,7 @@ environments {
 			username = "sa"
 			password = ""
 			dbCreate = "update"
-			url = "jdbc:h2:mem:payway;MVCC=TRUE;LOCK_TIMEOUT=10000"
+			url = "jdbc:h2:mem:payway;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
         }
 	}
 }
