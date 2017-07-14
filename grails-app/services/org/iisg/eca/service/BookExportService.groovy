407d0ca97053dbe686c4d9d84aff38e8ede1e655
Gordan Cupac <gordan-cupac@users.noreply.github.com>
2017-07-14 10:12:41 +0200

    added organisation to xml program book export (2)

--- grails-app/services/org/iisg/eca/service/BookExportService.groovy
+++ grails-app/services/org/iisg/eca/service/BookExportService.groovy
@@ -269,7 +269,11 @@ class BookExportService {
                 if (type.id != ParticipantType.CO_AUTHOR) {
                     builder."${type.type.toLowerCase()}s" {
                         users.each { user ->
-                            builder."${type.type.toLowerCase()}name"("$user.firstName $user.lastName")
+                            builder."${type.type.toLowerCase()}name"("$user.firstName $user.lastName") // DEPRECATED
+                            builder."${type.type.toLowerCase()}" {
+                                builder.name("$user.firstName $user.lastName")
+                                builder.organisation("$user.organisation")
+                            }
                         }
                     }
                 }
