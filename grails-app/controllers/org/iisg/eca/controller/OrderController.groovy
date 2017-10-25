672ffaf3dbca439f346c47a317bd22db034f7dea
Gordan Cupac <gordan-cupac@users.noreply.github.com>
2017-10-25 15:42:05 +0200

    added logging order controller

--- grails-app/controllers/org/iisg/eca/controller/OrderController.groovy
+++ grails-app/controllers/org/iisg/eca/controller/OrderController.groovy
@@ -13,35 +13,62 @@ class OrderController {
     def emailCreationService
 
     def post() {
+        log.error('aaa1')
+
         Map<String, Object> queryParams = WebUtils.fromQueryString(request.queryString)
         PayWayMessage message = new PayWayMessage(queryParams)
+
+        log.error('aaa2')
+
         if (message.containsKey('POST')) {
+
+            log.error('aaa3')
+
             if (message.isValid()) {
-                boolean insert = false
+
+                log.error('aaa4')
+
+                boolean insert = false // is it a new order
                 long orderId = new Long(message.get('orderid').toString())
+                log.error ('orderId: ' + orderId )
 
                 // Obtain the order (or create a new order) and refresh
                 Order order = Order.get(orderId)
                 if (!order) {
+                    log.error('new Order()')
                     order = new Order()
                     order.setId(orderId)
-                    insert = true
+                    insert = true // is a new order
                 }
                 order.refreshOrder(insert)
 
+                log.error ('orderId (2): ' + orderId )
+
                 // If the payment is accepted and we know the participant, sent the payment accepted email
-                if ((order.getPayed() == Order.PAYMENT_ACCEPTED) && order.participantDate) {
+                log.error('order.getPayed(): ' + order.getPayed())
+                log.error('order.participantDate: ' + order.participantDate)
+                if ( (order.getPayed() == Order.PAYMENT_ACCEPTED) && order.participantDate ) {
+                    log.error('aaa5a');
+
                     SentEmail email = emailCreationService.createPaymentAcceptedEmail(order.participantDate.user, order)
                     emailService.sendEmail(email)
                 }
-                else if (!order.participantDate) {
-                    log.warn('Unknown participant for order ' + order.id)
+                else {
+                    log.error('aaa5b');
+                    if (!order.participantDate) {
+                        log.error('aaa5b1');
+                        log.warn('Unknown participant for order ' + order.id)
+                    } else {
+                        log.error('aaa5b2');
+                    }
                 }
 
                 render(text: 'OK')
                 return
             }
         }
+
+        log.error('aaaError');
         response.sendError(400)
     }
 }
