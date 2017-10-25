f898445ae0e1ee5a1e9dc92e2607518dd7a9f8f4
kerim1 <kerim1@users.noreply.github.com>
2017-10-25 13:25:05 +0200

    log

--- grails-app/controllers/org/iisg/eca/controller/OrderController.groovy
+++ grails-app/controllers/org/iisg/eca/controller/OrderController.groovy
@@ -16,12 +16,12 @@ class OrderController {
         Map<String, Object> queryParams = WebUtils.fromQueryString(request.queryString)
         PayWayMessage message = new PayWayMessage(queryParams)
         if (message.containsKey('POST') && message.isValid()) {
-            log.warn('Start 1')
+            log.error('Start 1')
 
             boolean insert = false
             long orderId = new Long(message.get('orderid').toString())
 
-            log.warn('Start 2')
+            log.error('Start 2')
 
             // Obtain the order (or create a new order) and refresh
             Order order = Order.get(orderId)
@@ -30,20 +30,20 @@ class OrderController {
                 order.setId(orderId)
                 insert = true
             }
-            log.warn('Refresh order')
+            log.error('Refresh order')
             order.refreshOrder(insert)
 
             // If the payment is accepted and we know the participant, sent the payment accepted email
             if ((order.getPayed() == Order.PAYMENT_ACCEPTED) && order.participantDate) {
-                log.warn('Send mail!')
+                log.error('Send mail!')
                 SentEmail email = emailCreationService.createPaymentAcceptedEmail(order.participantDate.user, order)
                 emailService.sendEmail(email)
             }
             else if (!order.participantDate) {
-                log.warn('Unknown participant for order ' + order.id)
+                log.error('Unknown participant for order ' + order.id)
             }
 
-            log.warn('OK!')
+            log.error('OK!')
             render(text: 'OK')
             return
         }
