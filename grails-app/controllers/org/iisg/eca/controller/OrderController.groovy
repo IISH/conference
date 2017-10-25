5ea0b1ba51ef5b3775edf7deb7cd2fc786543a58
kerim1 <kerim1@users.noreply.github.com>
2017-10-25 14:39:14 +0200

    Fix if statement

--- grails-app/controllers/org/iisg/eca/controller/OrderController.groovy
+++ grails-app/controllers/org/iisg/eca/controller/OrderController.groovy
@@ -15,30 +15,32 @@ class OrderController {
     def post() {
         Map<String, Object> queryParams = WebUtils.fromQueryString(request.queryString)
         PayWayMessage message = new PayWayMessage(queryParams)
-        if (message.containsKey('POST') && message.isValid()) {
-            boolean insert = false
-            long orderId = new Long(message.get('orderid').toString())
+        if (message.containsKey('POST')) {
+            if (message.isValid()) {
+                boolean insert = false
+                long orderId = new Long(message.get('orderid').toString())
 
-            // Obtain the order (or create a new order) and refresh
-            Order order = Order.get(orderId)
-            if (!order) {
-                order = new Order()
-                order.setId(orderId)
-                insert = true
-            }
-            order.refreshOrder(insert)
+                // Obtain the order (or create a new order) and refresh
+                Order order = Order.get(orderId)
+                if (!order) {
+                    order = new Order()
+                    order.setId(orderId)
+                    insert = true
+                }
+                order.refreshOrder(insert)
 
-            // If the payment is accepted and we know the participant, sent the payment accepted email
-            if ((order.getPayed() == Order.PAYMENT_ACCEPTED) && order.participantDate) {
-                SentEmail email = emailCreationService.createPaymentAcceptedEmail(order.participantDate.user, order)
-                emailService.sendEmail(email)
-            }
-            else if (!order.participantDate) {
-                log.warn('Unknown participant for order ' + order.id)
-            }
+                // If the payment is accepted and we know the participant, sent the payment accepted email
+                if ((order.getPayed() == Order.PAYMENT_ACCEPTED) && order.participantDate) {
+                    SentEmail email = emailCreationService.createPaymentAcceptedEmail(order.participantDate.user, order)
+                    emailService.sendEmail(email)
+                }
+                else if (!order.participantDate) {
+                    log.warn('Unknown participant for order ' + order.id)
+                }
 
-            render(text: 'OK')
-            return
+                render(text: 'OK')
+                return
+            }
         }
         response.sendError(400)
     }
