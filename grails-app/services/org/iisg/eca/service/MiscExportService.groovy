968b0cc2a9b8b6f26e4629bb705ea5d98a09162d
G. Cupac <gordan.cupac@di.huc.knaw.nl>
2021-08-12 17:45:08 +0200

    Fix getParticipants (3)

--- grails-app/services/org/iisg/eca/service/MiscExportService.groovy
+++ grails-app/services/org/iisg/eca/service/MiscExportService.groovy
@@ -49,17 +49,16 @@ class MiscExportService {
 
 		// Transform the participant states into a map: [1,2] => {"0": 1, "1": 2}
 //		participantStatesMap = participantStates.withIndex().collectEntries { stateId, id -> [id.toString(), stateId] }
-//		participantStatesMap = participantStates.eachWithIndex().collectEntries { stateId, id -> [id.toString(), stateId] }
+		participantStatesMap = participantStates.eachWithIndex().collectEntries { stateId, id -> [id.toString(), stateId] }
 
 		// Obtain results and transform them
 		Sql sql = new Sql(dataSource)
 		String sqlQuery = ACTIVE_PARTICIPANTS_SQL.replace(':status',
-				StringUtils.join(participantStates, ',')
+				participantStatesMap.keySet().collect { ":$it" }.join(',')
 		)
-//				participantStatesMap.keySet().collect { ":$it" }.join(',')
 
 		// Query the database and create the export
-		List<Map> results = sql.rows(sqlQuery, [dateId: pageInformation.date.id] + participantStatesMap)
+		List<Map> results = sql.rows(sqlQuery, [dateId: pageInformation.date.id])
 
 		// Create XLS export
 		String title = messageSource.getMessage('participantDate.participants.label', null, LocaleContextHolder.locale)
